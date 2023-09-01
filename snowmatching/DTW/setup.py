adcea39ae6f0aecdbeb668151978d4bb225c6ba2
Leo VIALLON-GALINIER <leo.viallon@meteo.fr>
2023-09-01 10:36:30 +0200

    Update version and update install tools
    Update install tools to reflect deprecation of distutils.
    
    Update the version following previous commit

--- snowmatching/DTW/setup.py
+++ snowmatching/DTW/setup.py
@@ -4,25 +4,23 @@ Created on 24 aout 2016
 @author: hagenmullerp
 '''
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from setuptools import setup, Extension
+from Cython.Build import build_ext
 
 import numpy
-#remove openmp if non parallel
-ext_modules=[
+# remove openmp if non parallel
+ext_modules = [
     Extension("DTW_CCore",
               ["DTW_CCore.pyx"],
               libraries=["m"],
               # Be careful -ffast-math disables nan checks, including isnan function
-              extra_compile_args = ["-O3", "-march=native", "-fopenmp" ],
+              extra_compile_args = ["-O3", "-march=native", "-fopenmp"],
               include_dirs=[numpy.get_include()],
-              #extra_link_args=['-fopenmp'] #To remove if non parallel
-              ) 
+              # extra_link_args=['-fopenmp'] #To remove if non parallel
+              )
 ]
 
-setup( 
-  name = "DTW_CCore",
-  cmdclass = {"build_ext": build_ext},
-  ext_modules = ext_modules
-)
+setup(name = "DTW_CCore",
+      cmdclass = {"build_ext": build_ext},
+      ext_modules = ext_modules
+      )
