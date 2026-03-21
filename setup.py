d07bc76bd3c2e6863cba548ed25a8ba035fb33da
Leo VIALLON-GALINIER <leo.viallon@meteo.fr>
2026-03-21 09:55:10 +0100

    Add pyproject.toml and update install process

--- setup.py
+++ setup.py
@@ -7,21 +7,9 @@ from Cython.Build import build_ext
 
 import numpy
 
-HERE = os.path.dirname(os.path.realpath(__file__))
-
-PYTHON_MIN_VERSION = (3, 6)
-
-packages = ['snowmatching', 'snowmatching/DTW']
+_here = os.path.dirname(os.path.realpath(__file__))
 
 setup(name='Snowmatching',
-      version='0.2',
-      description="Snowmatching is a collection of tools to match snow profiles.",
-      long_description="""
-      Snowmatching is a collection of tools to match snow profiles against a refernce
-      profile for further use in comparisons of snow profiles from different sources,
-      do clustering or define.
-      """,
-
       # Building the DTW C Core
       ext_modules=[
           Extension("snowmatching.DTW.DTW_CCore",
@@ -33,23 +21,4 @@ setup(name='Snowmatching',
                     )
          ],
       cmdclass = {"build_ext": build_ext},
-
-      # Python part of the code
-      packages=packages,
-      classifiers=[
-          'Operating System :: POSIX :: Linux',
-
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-
-          'Topic :: Scientific/Engineering',
-          ],
-      install_requires=[
-           'numpy',
-           'scipy',
-          ],
-      python_requires='>=' + '.'.join(str(n) for n in PYTHON_MIN_VERSION),
       )
